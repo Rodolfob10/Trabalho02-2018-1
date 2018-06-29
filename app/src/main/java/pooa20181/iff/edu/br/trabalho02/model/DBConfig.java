70ed7ac61d0d4a88709ad1d7bab33c4ad5414aae
Rodolfob10 <rodolfobsilva10@gmail.com>
2018-06-29 20:10:39 -0300

    Realm

---
+++ app/src/main/java/pooa20181/iff/edu/br/trabalho02/model/DBConfig.java
@@ -0,0 +1,24 @@
+package pooa20181.iff.edu.br.trabalho02.model;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class DBConfig extends Application{
+
+            public void onCreate(){
+                super.onCreate();
+
+                       Realm.init(getApplicationContext());
+
+                        RealmConfiguration.Builder builder = new RealmConfiguration.Builder();
+                        builder.name("eleicao.realm");
+                        builder.schemaVersion(0);
+                         builder.deleteRealmIfMigrationNeeded();
+                        RealmConfiguration realmConfiguration = builder.build();
+                        Realm.setDefaultConfiguration(realmConfiguration);
+
+
+                }
+}
